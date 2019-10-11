ea2d71e1eca1a0cc5d2653ce9831bab7e18e69a6
EugenBudanow <budanov.e@gmail.com>
2019-10-12 00:46:08 +0500

    переписал вспомогательные функции, поправил некоторые баги, добавил комментарии

--- web/views.py
+++ web/views.py
@@ -182,45 +182,46 @@ def operations():
         pass
     return render_template("operations.html", form=form, operations=user_operations)
 
-# методы ниже лучше разместить в модели? если да то как из модели делать запросы на получение данных? (ну либо мб просто в отдельный файл вынести)
-def get_user_accs(user_id):
-    result = session.query(Account).filter(Account.id_user == user_id).all()
-    # нужно переделать запрос, чтобы  он возвращал не весь счет, а только нужные нам данные, добавить проверку is_actual и сортировку (в порядке чего?)
-    if len(result) == 0:  # not len(result) ?
-        return []  # если у пользователя нет счетов, возвращаем пустой список
-    return [(account.id, account.name) for account in result] # переделать остальные функции под такой формат
+
+"""
+
+методы ниже оставить здесь, в модели добавить или в отдельный файл?
+Хотел в модели, но почему то подумал, что из модели делать запрос в базу не очень красиво
+
+"""
 
 
-def get_user_categories(user_id):
-    categories = []
-    result = session.query(Category).filter(Category.id_user == user_id).all()
-    # тоже самое, что и в get_user_accs и сделалать вывод в виде дерева категорий
-    if len(result) == 0:
-        return categories
-    for category in result:
-        categories.append((category.id, category.name))
-    return categories
+def get_user_accs(id_user):
+    result = session.query(Account.id, Account.name).filter(Account.id_user == id_user).all()
+    if len(result):
+        return [(account.id, account.name) for account in result]
+    return list  # wtfforms чтобы отрисовать форму требует список, даже если он пустой
 
 
-def get_user_operations(user_id):
-    user_accounts_id = []
-    user_accounts = get_user_accs(user_id)
-    if len(user_accounts) == 0:
+def get_user_categories(id_user):  # добавить вывод в виде дерева
+    result = session.query(Category.id, Category.name).filter(Category.id_user == id_user).all()
+    if result:
+        return [(category.id, category.name) for category in result]
+    return list
+
+
+def get_user_tags(id_user):
+    result = session.query(Tag.id, Tag.name).filter(Tag.id_user == id_user, Tag.is_actual == True).all()
+    if result:
+        return [(user_tags.id, user_tags.name) for user_tags in result]
+    return list
+
+
+def get_user_operations(id_user):
+    user_accounts = get_user_accs(id_user)
+    if not user_accounts:
         return
-    for account in user_accounts:
-        user_accounts_id.append(account[0])
+    # в user_accounts список [(id, название), (id, название)], нам нужны только id
+    user_accounts_id = []  # как это написать в одну строку?
+    user_accounts_id = [account[0] for account in user_accounts]
+    # В данный момент, вместо названий операций и категорий выводятся теги, возможно ли с помощью sql запроса, сразу выдирать названия или лучше использовать другой способ?
     operations = session.query(Operation).filter(Operation.id_account.in_(user_accounts_id)).order_by(Operation.creation_time.desc()).all()
     return operations
-
-
-def get_user_tags(user_id):
-    user_tags = []
-    result = session.query(Tag).filter(Tag.id_user == user_id, Tag.is_actual == True).all()
-    if len(result) == 0:  # смотри комментарии get_user_accs
-        return user_tags
-    for tag in result:
-        user_tags.append((tag.id, tag.name))
-    return user_tags
 
 
 def get_tag_obj(tag_id, user_id):
@@ -228,7 +229,7 @@ def get_tag_obj(tag_id, user_id):
     if query:
         return query  # Если тег существует, вернуть его объект
     else:
-        pass  # Если нет, добавить новый объект и вернуть его
+        pass  # Если нет, добавить новый объект и вернуть его, сейчас нет возможности добавить новый тег через операцию
 
 
 @app.route('/reports')
