9b37d61024a5dbc62274ad9751b5d07e2dbbf116
EugenBudanow <budanov.e@gmail.com>
2019-10-16 22:55:39 +0500

    дописал тест логин\логаут, тест добавления операции

--- tests/test_web.py
+++ tests/test_web.py
@@ -2,6 +2,19 @@ import pytest
 from web import app, init_db
 #from web.models import Account, Base, Category, Currency, User, Operation, Tag
 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:123@localhost:5432/test'
+app.config['WTF_CSRF_ENABLED'] = False
+app.config['USERNAME'] = "mytg"
+app.config['PASSWORD'] = "mytg"
+
+operation_form_testdata = {
+    "category": 9,
+    "account": 2,
+    "tags": 1,
+    "name": "Test",
+    "description": "test operation",
+    "value": 111,
+}
 
 @pytest.fixture
 def client():
@@ -20,7 +33,7 @@ def test_login_page(client):
 
 def login(client, username, password):
     return client.post('/login', data=dict(
-        username=username,
+        telegram=username,
         password=password
     ), follow_redirects=True)
 
@@ -29,16 +42,28 @@ def logout(client):
     return client.get('/logout', follow_redirects=True)
 
 
-# def test_login_logout(client):
-#     # проверка того, что логин и логаут работает
-#     rv = login(client, app.config['USERNAME'], app.config['PASSWORD'])
-#     assert b'operations' in rv.data
+def test_login_logout(client):
+    # проверка того, что логин и логаут работает
+    
+    rv = login(client, app.config['USERNAME'], app.config['PASSWORD'])
+    assert 'Главная страница' in rv.get_data(as_text=True)
+    
+    rv = logout(client)
+    assert 'Авторизация' in rv.get_data(as_text=True)
 
-#     rv = logout(client)
-#     assert b'login_form' in rv.data
+    rv = login(client, app.config['USERNAME'] + 'x', app.config['PASSWORD'])
+    assert 'Неправильный логин/пароль' in rv.get_data(as_text=True)
 
-#     rv = login(client, app.config['USERNAME'] + 'x', app.config['PASSWORD'])
-#     assert b'Invalid username' in rv.data
+    rv = login(client, app.config['USERNAME'], app.config['PASSWORD'] + 'x')
+    assert 'Неправильный логин/пароль' in rv.get_data(as_text=True)
 
-#     rv = login(client, app.config['USERNAME'], app.config['PASSWORD'] + 'x')
-#     assert b'Invalid password' in rv.data
+
+def test_operation_page(client):
+    login(client, app.config['USERNAME'], app.config['PASSWORD'])
+    # Страница загрузилась, форма доступна (нужно задать форме уникальный css класс, чтобы точно знать, что это нужная форма)
+    rv = client.get('/operations', follow_redirects=True)
+    assert '<h1>Операции</h1>', '<div class="form-group  required">' in rv.get_data(as_text=True)
+
+    #Тест добавления операции
+    query = client.post('/operations', data=operation_form_testdata, follow_redirects=True)
+    assert 'Операция добавлена' in query.get_data(as_text=True)
