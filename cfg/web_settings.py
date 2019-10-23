612aa82adf17711ba994602e584273086c9f82b8
Nikita Yakovlev <nikita.uventa@gmail.com>
2019-10-23 22:01:10 +0300

    cfg add local_settings

--- cfg/web_settings.py
+++ cfg/web_settings.py
@@ -1,18 +1,17 @@
 import os
 from datetime import timedelta
 
+from .local_settings import TEST_DB_URI, DB_URI
+
 
 class Config:
-    """ Намеренно оставляю нерабочие (для меня) параметры в конфиге
-        Чтобы убедиться, что тесты заработали, и мы работаем с правильной базой
-    """
     SECRET_KEY = os.urandom(32)
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://postgres111:12113@localhost:5432/test'
+    SQLALCHEMY_DATABASE_URI = DB_URI
     REMEMBER_COOKIE_DURATION = timedelta(days=5)
 
 
 class TestConfig:
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://test2:111@localhost:5432/test2'
+    SQLALCHEMY_DATABASE_URI = TEST_DB_URI
     WTF_CSRF_ENABLED = False
     TESTING = True
     # BASEDIR = os.path.abspath(os.path.dirname(__file__))  # зачем эта строка? без нее все работает
